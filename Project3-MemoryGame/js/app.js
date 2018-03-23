d6a276ceb97ad62ff8665583da76207b99d4566e
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-03-23 14:09:42 +0800

    Matching click event handler functioning version

--- Project3-MemoryGame/js/app.js
+++ Project3-MemoryGame/js/app.js
@@ -1,19 +1,29 @@
 /*
  * Create a list that holds all of your cards
  */
-var listOfCards, shuffledListOfCards, mappedListOfCards, listOfOpenCards, moveCounter, matchCounter, card, userRating;
+var listOfCards, shuffledListOfCards, mappedListOfCards, listOfOpenCards, moveCounter, matchCounter, card, userRating, restartButton;
 
 //list of 16 elements, representing the 16 cards, the numbers 1-8 represent the 8 different figures (2 of each)
 listOfCards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
 listOfOpenCards = [];
+let minutes = 0;
+let seconds = 0;
 star1 = document.getElementById("firststar");
 star2 = document.getElementById("secondstar");
 star3 = document.getElementById("thirdstar");
+restartButton = document.getElementById("restart");
 
+//defining variables containing the font awesome classes for the 8 different card types
+const figures = ["fa-leaf", "fa-bicycle",
+ "fa-diamond", "fa-bomb",
+ "fa-bolt", "fa-anchor",
+ "fa-paper-plane-o", "fa-cube"];
 moves = document.getElementById("moves");
 //shuffle function
 var shuffle = function () {
     shuffledListOfCards = shuffleCards(listOfCards);
+    //the array of the randomly shuffled figures
+    mappedListOfCards = shuffledListOfCards.map(x => figures[x - 1]);
     return shuffledListOfCards;
 }
 //function intializing when the page starts
@@ -22,14 +32,9 @@ moveCounter = 0;
 matchCounter = 0;
 userRating = "Regular";
 
-//defining variables containing the font awesome classes for the 8 different card types
-const figures = ["fa-leaf", "fa-bicycle",
- "fa-diamond", "fa-bomb",
- "fa-bolt", "fa-anchor",
- "fa-paper-plane-o", "fa-cube"];
 
-//the array of the randomly shuffled figures
-mappedListOfCards = shuffledListOfCards.map(x => figures[x - 1]);
+
+
 
 // Loops through each card's ID and assign them the corresponding figure class to it's HTML.
 function assignCardPictures() {
@@ -72,19 +77,19 @@ var cards = document.querySelectorAll('.card');
 function movesCounter() {
     moveCounter++;
     moves.textContent = moveCounter;
-    if (moveCounter >= 48) {
+    if (moveCounter >= 49) {
         star3.className = "fa fa-star-o";
-        userRating = "Card Matching Hero"
+        userRating = "Master"
     }
-    if (moveCounter >= 48) {
+    if (moveCounter >= 65) {
         star2.className = "fa fa-star-o";
         userRating = "Expert"
     }
-    if (moveCounter >= 48) {
+    if (moveCounter >= 78) {
         star1.className = "fa fa-star-o";
         userRating = "Apprentice"
     }
-    console.log('movescounterevt');
+    console.log('3. Counter Moved');
     return userRating
 };
 
@@ -94,15 +99,17 @@ function setMatchClass() {
     openCards[1].classList.add("match");
     openCards[0].classList.remove("open", "show");
     openCards[1].classList.remove("open", "show");
-    console.log('MatchClassEvt');
+    console.log('5. Class has been matched');
+    clickFunctionsFinished = "yes";
+    console.log('6. After matching click function set to' +clickFunctionsFinished);
 
 };
 
 function winningCheck() {
     matchCounter++;
-    console.log('winningcheckrun');
+    console.log('6. winning check has been run');
     if (matchCounter === 2) {
-        swal("Won the game!", {
+        swal("You Won the game!", {
                 buttons: {
                     cancel: "Enough!",
                     OK: true,
@@ -112,6 +119,7 @@ function winningCheck() {
                 switch (value) {
                     case "OK":
                         swal("New Game Starting Right Now");
+                        restartGame();
                         break;
 
                     default:
@@ -125,7 +133,9 @@ function removeUnmatchedPairs() {
     const openCards = document.querySelectorAll(".open");
     openCards[0].classList.remove("open", "show");
     openCards[1].classList.remove("open", "show");
-    console.log('removePairsEvt');
+    clickFunctionsFinished = 'yes';
+    console.log(' 5. Click function set to' +clickFunctionsFinished+ 'after unmatched pair of cards detected');
+    console.log(clickFunctionsFinished);
 };
 
 /*function openCard(evt) {
@@ -138,20 +148,41 @@ function removeUnmatchedPairs() {
 function postpone(fun) {
     window.setTimeout(fun, 0);
 }*/
+function restartGame() {
+    listOfCards = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
+    listOfOpenCards = [];
+    cards.className = "";
+    shuffle();
+    moveCounter = 0;
+    moves.textContent = moveCounter;
+    matchCounter = 0;
+    userRating = "Jedi";
+    assignCardPictures();
+    minutes=0;
+    seconds=0;
+    console.log('RESTARTED');
+    };
+
+restartButton.addEventListener('click', restartGame);
+
+
 var clickFunctionsFinished = 'yes';
 var tempEventTarget = 0;
+
 deck.addEventListener('click', function (evt) {
     if (evt.target.nodeName === 'LI' && tempEventTarget !== evt.target && clickFunctionsFinished === 'yes') {
-        clickFunctionsFinished = 'nope';
+        console.log('1' + clickFunctionsFinished + 'in the beginning')
         tempEventTarget = evt.target;
         evt.target.classList.add("open", "show");
         listOfOpenCards.push(evt.target.firstElementChild.className);
-        console.log('OpenCardEvt');
+        console.log('2.Card Opening Happened');
 
         movesCounter();
 
         if (listOfOpenCards.length === 2) {
-            console.log('lengthistwonow');
+            clickFunctionsFinished = 'nope';
+            console.log('4.a clickfinished function set to' + clickFunctionsFinished +  'after length is 2')
+            console.log('4.b Array length is 2.');
             if (listOfOpenCards[0] === listOfOpenCards[1]) {
 
                 setMatchClass();
@@ -159,7 +190,7 @@ deck.addEventListener('click', function (evt) {
 
             } else {
 
-                setTimeout(removeUnmatchedPairs, 500);
+                setTimeout(removeUnmatchedPairs, 1000);
 
             }
 
@@ -167,10 +198,10 @@ deck.addEventListener('click', function (evt) {
 
         }
         
-    clickFunctionsFinished = 'yes';
         
-    }
-    
+        
+    } 
+    console.log('Section after if statement.')
     return tempEventTarget;
 })
 /*
@@ -193,10 +224,9 @@ deck.addEventListener('click', function (evt) {
  */
 
 
-var minutes = 0;
-var seconds = 0;
 
 function countTimer() {
+    
     startTime = setInterval(() => {
         time.innerText = minutes + ' : ' + seconds;
         seconds++;
