c3295b36ca50cd67b9b0d580295971a47d2cc473
tonytettinger <antal.tettinger@gmail.com>
2018-07-19 20:45:16 +0800

    Media query updates

--- Project6-RestaurantReviewApp/js/main.js
+++ Project6-RestaurantReviewApp/js/main.js
@@ -178,6 +178,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
+  more.classList.add("restaurant")
   li.append(more)
 
   return li
