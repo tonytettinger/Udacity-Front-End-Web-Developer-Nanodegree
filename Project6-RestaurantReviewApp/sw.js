39f78df8f700a0d96dd1d0ca2e1f1a3ef2b30492
Antal Tony Tettinger <antal.tettinger@gmail.com>
2018-07-21 01:22:37 +0800

    Service Worker finalized, working as expected.

--- Project6-RestaurantReviewApp/sw.js
+++ Project6-RestaurantReviewApp/sw.js
@@ -1,41 +1,36 @@
-const cacheName = "restaurantreviewv1";
+const cacheName = "rest1";
 const filesToCache = ["/"];
 
 self.addEventListener("install", function (event) {
     // Perform install steps
     console.log("[Serviceworker] Install");
     event.waitUntil(
-        caches.open(cacheName).then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log("[ServiceWorker] Caching app shell");
             return cache.addAll(filesToCache);
         })
     );
 });
 
-self.addEventListener('fetch', event => {
-    let request = event.request;
-
-    // ...
-
-    // Cache first, fall back to network or offline image
+self.addEventListener('fetch', function (event) {
     event.respondWith(
-        caches.match(request)
-        .then(response => {
-            return response || fetch(request)
-                .then(response => {
-                    // ...
-                    return response;
-                })
-                .catch(() => {
-                    // Offline fallback image
-                    if (request.url.match(/jpg70/gi)) {
-                        return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"><title id="offline-title">Offline</title><path fill="rgba(145,145,145,0.5)" d="M0 0h400v225H0z" /><text fill="rgba(0,0,0,0.33)" font-family="Helvetica Neue,Arial,sans-serif" font-size="27" text-anchor="middle" x="200" y="113" dominant-baseline="central">offline</text></svg>', {
-                            headers: {
-                                'Content-Type': 'image/svg+xml'
-                            }
-                        });
-                    }
-                });
+        caches.open('rest1').then(function (cache) {
+            return cache.match(event.request).then(function (response) {
+                return response || fetch(event.request).then(function (response) {
+                        cache.put(event.request, response.clone());
+                        return response;
+                    })
+                    .catch(() => {
+                        // Offline fallback image
+                        if (event.request.url.match(/jpg70/gi)) {
+                            return new Response('<svg role="img" aria-labelledby="offline-title" viewBox="0 0 400 225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice"><title id="offline-title">You are Offline</title><path fill="rgba(145,145,145,0.5)" d="M0 0h400v225H0z" /><text fill="rgba(0,0,0,0.33)" font-family="Helvetica Neue,Arial,sans-serif" font-size="27" text-anchor="middle" x="200" y="113" dominant-baseline="central">offline</text></svg>', {
+                                headers: {
+                                    'Content-Type': 'image/svg+xml'
+                                }
+                            });
+                        }
+                    });
+            });
         })
     );
 });
